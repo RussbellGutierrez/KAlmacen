ab34ef8b3f9397dffd92b0aa6804b584a9a81783
Russbell Gutierrez <gutierrezrussbell@gmail.com>
2020-01-08 14:18:06 -0500

    Actualizando 1.0.0

--- app/src/main/java/com/sistemas/gutierrez/kalmacen/MainActivity.kt
+++ app/src/main/java/com/sistemas/gutierrez/kalmacen/MainActivity.kt
@@ -2,11 +2,15 @@ package com.sistemas.gutierrez.kalmacen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG by lazy { MainActivity::class.java.simpleName }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.w(TAG,"Demo")
     }
 }
